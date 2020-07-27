da03ca24edf0fc1bd964981d8ff78c54f6126cd7
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-27 10:10:08 +0200

    some changes

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,22 +1,21 @@
 import React, {useRef, useState} from "react";
 
 import "./styles.css";
-import {Chevron} from "../Shevron";
+import {Chevron} from "../Chevron";
 
-export function Accordion(props) {
-    console.log('Accordion props', props);
+export function Accordion({title, content}) {
 
     const [setActive, setActiveState] = useState("");
     const [setHeight, setHeightState] = useState("0");
     const [setRotate, setRotateState] = useState("accordion__icon");
 
 
-    const content = useRef(null);
+    const contentRef = useRef(null);
 
     function toggleAccordion() {
         setActiveState(setActive === "" ? "active" : "");
         setHeightState(
-            setActive === "active" ? "0" : `${content.current.scrollHeight}px`
+            setActive === "active" ? "0" : `${contentRef.current.scrollHeight}px`
         );
         setRotateState(
             setActive === "active" ? "accordion__icon" : "accordion__icon rotate"
@@ -25,17 +24,17 @@ export function Accordion(props) {
     return (
         <div className="accordion__section">
             <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
-                <p className="accordion__title">{props.title}</p>
+                <p className="accordion__title">{title}</p>
                 <Chevron className={`${setRotate}`} width={10} fill={"#777"} />
             </button>
             <div
-                ref={content}
+                ref={contentRef}
                 style={{ maxHeight: `${setHeight}` }}
                 className="accordion__content"
             >
                 <div
                     className="accordion__text"
-                    dangerouslySetInnerHTML={{ __html: props.content }}
+                    dangerouslySetInnerHTML={{ __html: content }}
                 />
             </div>
         </div>
